319acc6572321cb3f8329c2be735469b953be4b3
Adam Afriansyah <adamafriansyahb@gmail.com>
2021-01-19 17:00:22 +0700

    Create Author routes

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 
-
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
 const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors');
 
 mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection;
@@ -23,8 +25,10 @@ app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: false, limit: '10mb'}));
 
 app.use('/', indexRouter);
+app.use('/authors', authorRouter);
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('server is running...');
